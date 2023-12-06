abdbb88ce67623ee35d8d5973aeae022f09e281a
gsoisson <107640837+gsoisson@users.noreply.github.com>
2023-12-06 23:50:39 +0100

    added all the files

---
+++ utils.py
@@ -0,0 +1,246 @@
+import random
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+import numpy as np
+import pandas as pd
+import torch
+
+
+def get_experiences(memory_buffer, minibatch_size):
+    """
+    Returns a random sample of experience tuples drawn from the memory buffer.
+
+    Retrieves a random sample of experience tuples from the given memory_buffer and
+    returns them as Torch Tensors. The size of the random sample is determined by
+    the mini-batch size (minibatch_size). 
+    
+    Args:
+        memory_buffer (deque):
+            A deque containing experiences. The experiences are stored in the memory
+            buffer as namedtuples: namedtuple("Experience", field_names=["state",
+            "action", "reward", "next_state", "terminated"]).
+
+    Returns:
+        A tuple (states, actions, rewards, next_states, terminated_vals) where:
+
+            - states are the starting states of the agent.
+            - actions are the actions taken by the agent from the starting states.
+            - rewards are the rewards received by the agent after taking the actions.
+            - next_states are the new states of the agent after taking the actions.
+            - terminated_vals are the boolean values indicating if the episode ended.
+
+        All tuple elements are TensorFlow Tensors whose shape is determined by the
+        mini-batch size and the given Gym environment. For the Lunar Lander environment
+        the states and next_states will have a shape of [MINIBATCH_SIZE, 8] while the
+        actions, rewards, and terminated_vals will have a shape of [MINIBATCH_SIZE]. All
+        TensorFlow Tensors have elements with dtype=torch.float32.
+    """
+
+    experiences = random.sample(memory_buffer, k=minibatch_size)
+
+    states = torch.tensor(
+        np.array([e.state for e in experiences if e is not None]),
+        dtype=torch.float32
+    )
+    actions = torch.tensor(
+        np.array([e.action for e in experiences if e is not None]),
+        dtype=torch.float32
+    )
+    rewards = torch.tensor(
+        np.array([e.reward for e in experiences if e is not None]),
+        dtype=torch.float32
+    )
+    next_states = torch.tensor(
+        np.array([e.next_state for e in experiences if e is not None]),
+        dtype=torch.float32
+    )
+    terminated_vals = torch.tensor(
+        np.array([e.terminated for e in experiences if e is not None]).astype(np.uint8),
+        dtype=torch.float32
+    )
+
+    return (states, actions, rewards, next_states, terminated_vals)
+
+
+def check_update_conditions(t, num_steps_upd, memory_buffer, minibatch_size):
+    """
+    Determines if the conditions are met to perform a learning update.
+
+    Checks if the current time step t is a multiple of num_steps_upd and if the
+    memory_buffer has enough experience tuples to fill a mini-batch (for example, if the
+    mini-batch size is 64, then the memory buffer should have more than 64 experience
+    tuples in order to perform a learning update).
+    
+    Args:
+        t (int):
+            The current time step.
+        num_steps_upd (int):
+            The number of time steps used to determine how often to perform a learning
+            update. A learning update is only performed every num_steps_upd time steps.
+        memory_buffer (deque):
+            A deque containing experiences. The experiences are stored in the memory
+            buffer as namedtuples: namedtuple("Experience", field_names=["state",
+            "action", "reward", "next_state", "terminated"]).
+
+    Returns:
+       A boolean that will be True if conditions are met and False otherwise. 
+    """
+
+    if (t + 1) % num_steps_upd == 0 and len(memory_buffer) > minibatch_size:
+        return True
+    else:
+        return False
+
+
+def get_new_eps(epsilon, eps_min, eps_decay):
+    """
+    Updates the epsilon value for the ε-greedy policy.
+    
+    Gradually decreases the value of epsilon towards a minimum value (E_MIN) using the
+    given ε-decay rate (E_DECAY).
+
+    Args:
+        epsilon (float):
+            The current value of epsilon.
+
+    Returns:
+       A float with the updated value of epsilon.
+    """
+
+    return max(eps_min, eps_decay * epsilon)
+
+
+def get_action(q_values, num_actions, epsilon=0.0):
+    """
+    Returns an action using an ε-greedy policy.
+
+    This function will return an action according to the following rules:
+        - With probability epsilon, it will return an action chosen at random.
+        - With probability (1 - epsilon), it will return the action that yields the
+        maximum Q value in q_values.
+    
+    Args:
+        q_values (torch.Tensor):
+            The Q values returned by the Q-Network. For the Lunar Lander environment
+            this TensorFlow Tensor should have a shape of [1, 4] and its elements should
+            have dtype=torch.float32. 
+        epsilon (float):
+            The current value of epsilon.
+
+    Returns:
+       An action (numpy.int64). For the Lunar Lander environment, actions are
+       represented by integers in the closed interval [0,3].
+    """
+
+    if random.random() > epsilon:
+        return torch.argmax(q_values[0]).item()
+    else:
+        return random.choice(np.arange(num_actions))
+
+
+def update_target_network(q_network, target_q_network, tau):
+    """
+    Updates the weights of the target Q-Network using a soft update.
+    
+    The weights of the target_q_network are updated using the soft update rule:
+    
+                    w_target = (TAU * w) + (1 - TAU) * w_target
+    
+    where w_target are the weights of the target_q_network, TAU is the soft update
+    parameter, and w are the weights of the q_network.
+    
+    Args:
+        q_network (nn.Module): 
+            The Q-Network. 
+        target_q_network (nn.Module):
+            The Target Q-Network.
+    """
+
+    with torch.no_grad():
+        for target_param, local_param in zip(target_q_network.parameters(), q_network.parameters()):
+            target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
+
+
+def plot_history(point_history, **kwargs):
+    """
+    Plots the total number of points received by the agent after each episode together
+    with the moving average (rolling mean). 
+
+    Args:
+        point_history (list):
+            A list containing the total number of points the agent received after each
+            episode.
+        **kwargs: optional
+            window_size (int):
+                Size of the window used to calculate the moving average (rolling mean).
+                This integer determines the fixed number of data points used for each
+                window. The default window size is set to 10% of the total number of
+                data points in point_history, i.e. if point_history has 200 data points
+                the default window size will be 20.
+            lower_limit (int):
+                The lower limit of the x-axis in data coordinates. Default value is 0.
+            upper_limit (int):
+                The upper limit of the x-axis in data coordinates. Default value is
+                len(point_history).
+            plot_rolling_mean_only (bool):
+                If True, only plots the moving average (rolling mean) without the point
+                history. Default value is False.
+            plot_data_only (bool):
+                If True, only plots the point history without the moving average.
+                Default value is False.
+    """
+
+    lower_limit = 0
+    upper_limit = len(point_history)
+
+    window_size = (upper_limit * 10) // 100
+
+    plot_rolling_mean_only = False
+    plot_data_only = False
+
+    if kwargs:
+        if "window_size" in kwargs:
+            window_size = kwargs["window_size"]
+
+        if "lower_limit" in kwargs:
+            lower_limit = kwargs["lower_limit"]
+
+        if "upper_limit" in kwargs:
+            upper_limit = kwargs["upper_limit"]
+
+        if "plot_rolling_mean_only" in kwargs:
+            plot_rolling_mean_only = kwargs["plot_rolling_mean_only"]
+
+        if "plot_data_only" in kwargs:
+            plot_data_only = kwargs["plot_data_only"]
+
+    points = point_history[lower_limit:upper_limit]
+
+    # Generate x-axis for plotting.
+    episode_num = [x for x in range(lower_limit, upper_limit)]
+
+    # Use Pandas to calculate the rolling mean (moving average).
+    rolling_mean = pd.DataFrame(points).rolling(window_size).mean()
+
+    plt.figure(figsize=(10, 7), facecolor="white")
+
+    if plot_data_only:
+        plt.plot(episode_num, points, linewidth=1, color="cyan")
+    elif plot_rolling_mean_only:
+        plt.plot(episode_num, rolling_mean, linewidth=2, color="magenta")
+    else:
+        plt.plot(episode_num, points, linewidth=1, color="cyan")
+        plt.plot(episode_num, rolling_mean, linewidth=2, color="magenta")
+
+    text_color = "black"
+
+    ax = plt.gca()
+    ax.set_facecolor("black")
+    plt.grid()
+    plt.xlabel("Episode", color=text_color, fontsize=30)
+    plt.ylabel("Total Points", color=text_color, fontsize=30)
+    yNumFmt = mticker.StrMethodFormatter("{x:,}")
+    ax.yaxis.set_major_formatter(yNumFmt)
+    ax.tick_params(axis="x", colors=text_color)
+    ax.tick_params(axis="y", colors=text_color)
+    plt.show()
